b85ca5135bccdcad787e30a58b36a847349f3153
Bernie Stern <bernzs@gmail.com>
2017-09-26 11:37:49 -0400

    Upgraded to latest babel version, but webpack is now broken.

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 'use strict';
 
-require('babel/register')({});
+require('babel-register')({});
+require("babel-polyfill");
+
 
 var server = require('./server');
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"; // TODO only for development on localhost
 
-server.listen(PORT, function () {
-  console.log('Server listening on', PORT);
-});
+// server.listen(PORT, function (err) {
+//   if (err) {
+//     throw new Error(err);
+//   }
+//
+//   console.log('Server listening on', PORT);
+// });
