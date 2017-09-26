0e76b59240335721e2a304d2c61a034875bb80bc
Bernie Stern <bernzs@gmail.com>
2017-09-26 11:38:17 -0400

    webpack now works

--- webpack.config.js
+++ webpack.config.js
@@ -3,38 +3,28 @@ var webpack = require('webpack');
 
 module.exports = {
   entry:  [
-    'webpack-dev-server/client?http://127.0.0.1:8080/',
-    'webpack/hot/only-dev-server',
     './client'
   ],
   output: {
-    path:     path.join(__dirname, 'assets'),
+    path: path.join(__dirname, 'assets'),
     publicPath: "/assets/",
     filename: 'bundle.js'
   },
   resolve: {
-    modulesDirectories: ['node_modules', 'shared'],
-    extensions:         ['', '.js', '.jsx']
+    modules: ['node_modules', 'shared'],
+    extensions: ['.js', '.jsx']
   },
   module: {
-    loaders: [
+    rules: [
       {
         test:    /\.jsx?$/,
         exclude: /node_modules/,
-        loaders: ['react-hot', 'babel']
+        loaders: ['babel-loader']
       }
     ]
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin()
   ],
-  devtool: 'inline-source-map',
-  devServer: {
-    hot: true,
-    proxy: {
-      '*': 'http://127.0.0.1:' + (process.env.PORT || 3000)
-    },
-    host: '127.0.0.1'
-  }
+  devtool: 'inline-source-map'
 };
